a7ac348bcc4429dbb2e9b6f532a46114cb465fe3
Neil Mendoza <neil@isthis.gd>
2015-08-12 16:00:39 -0700

    changes to step one

--- stepOne/src/ofApp.cpp
+++ stepOne/src/ofApp.cpp
@@ -1,18 +1,49 @@
 #include "ofApp.h"
 
+const ofVec3f ofApp::BOX_DIMS = ofVec3f(50.f, 50.f, 50.f);
+
 //--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::setup()
+{
+    // set some openFrameworks settings
+    ofSetFrameRate(60);
+    ofBackground(0);
+    
+    // create a new box mesh that is the dimensions that we have measured
+    boxMesh = ofMesh::box(BOX_DIMS.x, BOX_DIMS.y, BOX_DIMS.z);
+}
+
+//--------------------------------------------------------------
+void ofApp::update()
+{
 
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::draw(){
-
+void ofApp::draw()
+{
+    // start drawing the 3d scene from the point of view
+    // of the camera
+    camera.begin();
+    
+    // enable depth testing so that the box mesh masks
+    // the outline at the back of the box
+    ofEnableDepthTest();
+    
+    // draw our box mesh in dark grey
+    ofSetColor(20);
+    boxMesh.draw();
+    
+    // now draw a glowing green outline
+    ofSetColor(255);
+    boxMesh.drawWireframe();
+    
+    // disable depth testing
+    ofDisableDepthTest();
+    
+    // finish drawing the 3d scene from the point of view
+    // of the projector
+    camera.end();
 }
 
 //--------------------------------------------------------------
--- stepOne/src/ofApp.h
+++ stepOne/src/ofApp.h
@@ -1,22 +1,28 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxPostProcessing.h"
 
-class ofApp : public ofBaseApp{
+class ofApp : public ofBaseApp
+{
+public:
+    static const ofVec3f BOX_DIMS;
+    
+    void setup();
+    void update();
+    void draw();
 
-	public:
-		void setup();
-		void update();
-		void draw();
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
+private:
+    ofEasyCam camera;
+    ofVboMesh boxMesh;
 };
